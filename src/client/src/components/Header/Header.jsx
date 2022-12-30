48d1ba8bbb10746dac25eb24cb0e5f1cc539f7b3
PMX120202 <pmx12022002@gmail.com>
2022-12-30 18:36:49 +0700

    Change Search

--- src/client/src/components/Header/Header.jsx
+++ src/client/src/components/Header/Header.jsx
@@ -12,15 +12,23 @@ const Header = () => {
 		"http://localhost:8000/api/movies"
 	);
 
-	// function handleSearch (){
-	// 	if (!loading){
-	// 		return (
-					
-					
-	// 		)
-	// 	}
-	// }
-	// const temp = handleSearch()
+	function handleSearch (){
+		if (!loading){
+			return (
+				<ul className="ul-search">
+					{data.filter((item)=>
+						item.name.toLowerCase().includes(searchContent)
+					).map((item)=>(
+						<li key={item._id} className='search-item'>
+							<Link className="searchto-mv" to={`/movie/${item._id}`}>{item.name}</Link>
+						</li>
+					))
+					}
+				</ul>
+			)
+		}
+	}
+	const temp = handleSearch()
 	return (
 		<div className="header-container">
 			<div className="header">
@@ -29,21 +37,11 @@ const Header = () => {
 						<Link className="linkk" to={"/movie"}>Movie</Link>
 						<Link className="linkk" to={"/movie"}>News</Link>
 					</div>
-
-					<div className="search-container">
-						<div className="search">
-							<input type="text" placeholder="Tìm kiếm" className="search-input"  onChange={e => setSearchContent(e.target.value)} />
-							<div className="autocom-box">
-								{searchContent.length > 0 && (data.filter((item)=>
-									item.name.toLowerCase().includes(searchContent)
-									).map((item)=>(
-										<li key={item._id} className='search-item'>
-											<Link className="searchto-mv" to={`/movie/${item._id}`}>{item.name}</Link>
-										</li>
-								))) }
-							</div>
-							<button className="btn-search" ><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-							
+					<div className="search">
+						<input type="text" placeholder="Tìm kiếm" className="search-input"  onChange={e => setSearchContent(e.target.value)} />
+						<button className="btn-search" onClick={handleSearch}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+						<div>
+							{searchContent.length > 0 && temp }
 						</div>
 					</div>
 				</div>
@@ -54,3 +52,61 @@ const Header = () => {
 };
 
 export default Header;
+
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import "./header.css";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+// import { useState } from "react";
+// import useFetch from "../../hooks/useFetch";
+// const Header = () => {
+// 	const [searchContent,setSearchContent] = useState("")
+
+// 	const { data, loading, error } = useFetch(
+// 		"http://localhost:8000/api/movies"
+// 	);
+
+// 	// function handleSearch (){
+// 	// 	if (!loading){
+// 	// 		return (
+					
+					
+// 	// 		)
+// 	// 	}
+// 	// }
+// 	// const temp = handleSearch()
+// 	return (
+// 		<div className="header-container">
+// 			<div className="header">
+// 				<div className="headerList">
+// 					<div className="linkto">
+// 						<Link className="linkk" to={"/movie"}>Movie</Link>
+// 						<Link className="linkk" to={"/movie"}>News</Link>
+// 					</div>
+
+// 					<div className="search-container">
+// 						<div className="search">
+// 							<input type="text" placeholder="Tìm kiếm" className="search-input"  onChange={e => setSearchContent(e.target.value)} />
+// 							<div className="autocom-box">
+// 								{searchContent.length > 0 && (data.filter((item)=>
+// 									item.name.toLowerCase().includes(searchContent)
+// 									).map((item)=>(
+// 										<li key={item._id} className='search-item'>
+// 											<Link className="searchto-mv" to={`/movie/${item._id}`}>{item.name}</Link>
+// 										</li>
+// 								))) }
+// 							</div>
+// 							<button className="btn-search" ><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+							
+// 						</div>
+// 					</div>
+// 				</div>
+
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default Header;
